143f6419dcd7574f99548f2da395ea4fd57e4120
Novia Puspitasari <noviapuspitasari13@gmail.com>
2017-03-28 22:48:18 +0700

    Praktikum CRUD

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8888/codeigniter/';
-
+$config['base_url'] = 'http://localhost:8080/codeigniter_alpha/';
 /*
 |--------------------------------------------------------------------------
 | Index File
